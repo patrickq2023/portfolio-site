a14a7fa1872c771cb3075527a8ff4093f28e1045
patrickq2023 <151511696+patrickq2023@users.noreply.github.com>
2024-03-27 18:05:25 +0000

    adhustmets to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import myImage from '../assets/images/pq.jpg';
+// import myImage from '../assets/images/pq.jpg';
 import pq1Image from '../assets/images/pq1.jpg';
 import pq2Image from '../assets/images/pq2.jpg';
-import pq5Image from '../assets/images/pq5.jpg'
+import pq5Image from '../assets/images/pq5.jpg';
 
 const About = () => {
   return (
     <div className="about-heading-container">
       <h2>About Me</h2>
-      <div className="image-container">
-          <img src={pq5Image} alt="Myself" />
-        </div>
       <div className="about-container">
         <div className="paragraph-container">
+          <div className="image-container">
+            <img src={pq5Image} alt="Myself" />
+          </div>
+          <div>
           <p>
-            Hello! My name is Patrick, my friends call me pq, I'm a London based Junior Software Engineer.
-          </p>
-          <p>
+            Hello! My name is Patrick, my friends call me pq, I'm a London based Junior Software Engineer.  </p>    
+            <p>               
             Transitioning from a lifelong career as a professional photographer and business owner, I sought a change of direction. 
             Prompted by a friend to explore the Software Engineering Course at General Assembly, I enrolled to embark on a new journey. 
             I was immediately intrigued by software engineering, and the opportunity that gave me to acquire new skills and implement 
@@ -32,24 +32,25 @@ const About = () => {
             Please take a look at my projects and if you like what you see please feel free to get in touch.
           </p>                   
         </div>
-        <div className="image-container">
-          <img src={myImage} alt="Myself" />
         </div>
       </div>
       <div className="about-container">
         <div className="paragraph-container">
+          <div className="image-container">
+            <img src={pq1Image} alt="Cycling" />
+          </div>
           <p>
-          Originally from South Africa, I came to London after graduation for a working holiday. The plan was to stay for a year, do some work, 
-          see a bit Europe over the summer, come back to London, earn a bit more cash and be back home for Christmas. I never went back and have 
-          been in London ever since. I found work as a photgraphic assistant and within a few years had set up my own photographic business.
-        </p>
-        </div>
-        <div className="image-container">
-          <img src={pq1Image} alt="Cycling" />
+            Originally from South Africa, I came to London after graduation for a working holiday. The plan was to stay for a year, do some work, 
+            see a bit Europe over the summer, come back to London, earn a bit more cash and be back home for Christmas. I never went back and have 
+            been in London ever since. I found work as a photographic assistant and within a few years had set up my own photographic business.
+          </p>
         </div>
       </div>
       <div className="about-container">
         <div className="paragraph-container">
+          <div className="image-container">
+            <img src={pq2Image} alt="Instagram" />
+          </div>
           <p>  Outside of work I have plenty of other interests including travelling, enjoying great food, Formula1 and snowboarding. 
             I love to keep fit and am a very keen triathlete, but my one true love is cycling. I serve as Road Captain on the 
             committee of my local cycling club, coordinating and planning club rides, and ride out with them at least twice a week. 
@@ -61,9 +62,8 @@ const About = () => {
             Feel free to have a look at my personal Instagram to view a few of my adventures.
           </p>
         </div>
-        <div className="image-container">
-          <img src={pq2Image} alt="Instagram" />
-        </div>
+      </div>
+      <div className="about-container">         
       </div>
     </div>
   );
